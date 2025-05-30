5ab73e2cdf1fafb61ebe0aeb4f6fa0996b696fb6
cbeat0n <cbeaton@mail.bradley.edu>
2025-05-29 18:44:57 -0700

    Updated Chatbot

--- chatbot.py
+++ chatbot.py
@@ -10,35 +10,35 @@ def chatbot(severity):
     # Immediate Care
     # Emergency
 
-    if severity == "Safe":
+    if severity == 1:
         return f"""
-            You are experiencing a situation of {severity} severity. Please consider taking light
+            You are experiencing a situation of Safe severity. Please consider taking light
             doses of Ibuprofen, along with Gatorade / water , an icepack, and rest. Do not panic,
             everything is fine.
         """
-    elif severity == "Mild":
+    elif severity == 2:
         return f"""
-            You are experiencing a situation of {severity} severity. Please consider getting in
+            You are experiencing a situation of Mild severity. Please consider getting in
             contact with your primary doctor with in 1-2 weeks, as this situation may cause issues.
             In the mean time, do not panic. Set an appointment at speak with your primary care 
             doctor as soon as possible.
         """
-    elif severity == "Moderate":
+    elif severity == 3:
         return f"""
-            You are experiencing a situation of {severity} severity. Please consider getting in
+            You are experiencing a situation of Moderate severity. Please consider getting in
             contact with your primary doctor with in the next week, as this situation is severe.
             Set an appointment at speak with your primary care doctor immediately.
         """
-    elif severity == "Immediate Care":
+    elif severity == 4:
         return f"""
-            You are experiencing a situation of {severity} severity. You should seek urgent medical
+            You are experiencing a situation of Immediate Care severity. You should seek urgent medical
             attention at an Urgent Care clinic, preferably in the next day or two at the most. If 
             possible, have someone else drive you there or take a rideshare service and bring
             food and water so you can stay there. Do not panic, seek help.
         """
     else:
         return f""" 
-            You are experiencing a situation of {severity} severity. Do not panic. This is a major
+            You are experiencing a situation of Emergency severity. Do not panic. This is a major
             emergency. You need to call 911 or your local emergency services hotline to request an
             ambulance pickup or have a relative or close friend drive you to the nearest hospital.
 
